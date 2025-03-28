a1996a0be4362681b20de74fcaad3d3b93d5cbf9
Oldnavigator <simonmortsch@gmail.com>
2025-03-28 10:12:54 +0100

    Startseite

---
+++ startseite.php
@@ -0,0 +1,21 @@
+<?php
+if(){
+
+
+}else{
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Satrtseite</title>
+</head>
+<body>
+    <h1>Gamespace - Spieleliste</h1>
+</body>
+</html>
+
+<?php
+}
+?>
