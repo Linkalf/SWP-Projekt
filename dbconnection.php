484ee1e4af2cd1355934c5baa215b6b16e8ac1c3
Oldnavigator <simonmortsch@gmail.com>
2025-04-02 09:47:00 +0200

    datenbank verbindung

---
+++ dbconnection.php
@@ -0,0 +1,9 @@
+<?php
+try {
+    $dsn = "mysql:host=localhost;dbname=gamespace;charset=utf8";
+
+    $pdo = new PDO($dsn, 'root', '');
+} catch (PDOException $e) {
+    die("Fehler beim Verbindungsaufbau mit der Datenbank");
+}
+?>
