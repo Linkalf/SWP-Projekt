bb8b5c6ef336bf1d1024310c3431416377580172
Oldnavigator <simonmortsch@gmail.com>
2025-04-03 10:52:43 +0200

    starttseite überarbeiten

--- startseite.php
+++ startseite.php
@@ -12,30 +12,9 @@ if(){
     <title>Satrtseite</title>
 </head>
 <body>
-    <h1>Gamespace - Spieleliste</h1>
+    <h1>Gamespace - Startseite</h1>
 
     
-    <?php include('header.php'); ?>
-    <?php 
-        if(isset($_SESSION ['bname'])){
-            echo '<h1>Willkommen, ' . $_SESSION('username') . '!</h1>';
-        }else {
-            echo '<h1>Willkommen!</h1>';
-        }
-    ?>
-    <h1>Alle Spiele: </h1>
-
-    <?php if (count($results) > 0): ?>
-        <?php foreach ($results as $row): ?>
-
-            <h3><?php echo(htmlspecialchars($row['name'])); ?></h3>
-            <h3><?php echo(htmlspecialchars($row['entwickler'])); ?></h3>
-            <h3><?php echo(htmlspecialchars($row['release'])); ?></h3>
-
-        <?php endforeach; ?>
-    <?php else: ?>
-        <p>Noch keine Spiele gefunden</p>
-    <?php endif; ?>
 
 </body>
 </html>
