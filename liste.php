562c2c42ceb54efaccd14e9a98509f1c964e8918
Luciifea <reichmann.nathalie@hakspittal.at>
2025-04-03 12:00:02 +0200

    überschrift liste

--- liste.php
+++ liste.php
@@ -1,8 +1,5 @@
 <?php
-if(){
 
-
-}else{
 ?>
 <!DOCTYPE html>
 <html lang="de">
@@ -10,9 +7,61 @@ if(){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Satrtseite</title>
+
+
+    <style>
+
+        body{ 
+            background-color: black;
+            color: white
+            }
+
+        .header {
+            width: 1300px; 
+            height: 220px;
+            margin: 50px auto;
+            background: linear-gradient(0deg, #000, #333);
+            position: relative;
+        }
+
+        .header:before,
+        .header:after {
+            content: "";
+            position: absolute;
+            background: linear-gradient(45deg, red, orange, yellow, 
+            green, blue, indigo, violet);
+            z-index: -1;
+            width: calc(100% + 4px);
+            height: calc(100% + 4px);  
+            top: -2px;
+            left: -2px;
+            background-size: 500%;
+            animation: wandernderFarbverlauf 30s linear infinite;
+        }
+
+        .header:after {
+            filter: blur(25px);
+        }
+
+        @keyframes wandernderFarbverlauf {
+            0%   { background-position: 0 0 }
+            50%  { background-position: 500% 0 }
+            100% { background-position: 0 0 }
+        }
+
+        .headertext{
+            text-align: center;
+            padding-top: 50px;
+            font-size: 100px;
+        }
+
+    </style>
 </head>
 <body>
-    <h1>Gamespace - Spieleliste</h1>
+
+<div class="header">
+  <h1 class="headertext">Gamespace-Spieleliste</h1>
+</div>
 
     
     <?php include('header.php'); ?>
@@ -39,7 +88,3 @@ if(){
 
 </body>
 </html>
-
-<?php
-}
-?>
